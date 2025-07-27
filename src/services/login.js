d1f8b30b2c10c6adb9ae07929dce50cf1bc8737d
nyhvic <nyhvic@gmail.com>
2025-07-27 20:46:52 +0900

    로그인 토큰,정보 받기
    docker compose - 환경변수 추가(API키, redirect uri)
    dockerfile - 바꾼거없는데지혼자 커밋됨
    package - axios qs추가
    router 설정
    login - 토큰 받아유저 id 닉네임 불러오는거까지

---
+++ src/services/login.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const router = express.Router();
+const qs = require('qs');
+const axios = require('axios');
+
+const kakao = { //환경변수 불러옴
+    clientID: process.env.KAKAO_REST_API_KEY,
+    redirectURI: process.env.KAKAO_REDIRECT_URI
+};
+
+exports.kakaoLogin = async (req, res) => { //프론트에서 로그인 시 kakao/code로 redirect됨
+    const authcode = req.query.code;
+    let token;
+    try {
+        const tokenResponse = await axios.post( //token 받아오기
+            "https://kauth.kakao.com/oauth/token",
+            qs.stringify({
+                grant_type: "authorization_code",
+                client_id: kakao.clientID,
+                redirect_uri: kakao.redirectURI,
+                code: authcode
+            }),
+            {
+                headers: {
+                    "content-type": "application/x-www-form-urlencoded",
+                }
+            }
+        );
+        token = tokenResponse.data;
+    } catch (e) {
+        console.log(e);
+        console.log(kakao);
+        return res.status(500).json(e.response ? e.response.data : e.message);
+    }
+
+    let user;
+    try { //유저 정보 받아오기
+        const userResponse = await axios({
+            method: "GET",
+            url: "https://kapi.kakao.com/v2/user/me",
+            headers: {
+                "content-type": "application/x-www-form-urlencoded",
+                Authorization: `Bearer ${token.access_token}`,
+            },
+        });
+        user = userResponse.data;
+    } catch (e) {
+        console.log(e);
+        return res.status(500).json(e.response ? e.response.data : e.message);
+    }
+
+    //user.data 이용해서 뭔가뭔가 하기
+    console.log(user);
+    res.json(user);
+};
